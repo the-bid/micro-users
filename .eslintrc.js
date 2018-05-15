9beccfef4f6667a6167e38a630028b626212bf56
Daniel Biales <bialesdaniel@gmail.com>
2018-05-15 01:32:35 -0400

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,19 +9,11 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
   rules: {
-    // ES6
     'no-var': 'error',
     'prefer-const': 'error',
-    // 'prefer-template': 'error',
-    // 'object-shorthand': 'error',
-    // 'prefer-destructuring': 'error',
     'prefer-arrow-callback': 'error',
-
-    // Override
     'no-empty': ['warn', { allowEmptyCatch: true }],
     'no-unused-vars': ['error', { ignoreRestSiblings: true, argsIgnorePattern: 'info' }],
-
-    // Disable
     'no-console': 'warn'
   }
 }
